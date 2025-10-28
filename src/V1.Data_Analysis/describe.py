68a157af998699f3329e3d5208d1bb72c949b828
magnitopic <magnitopic@gmail.com>
2025-10-28 11:39:11 +0100

    chore: simplified requirements

--- src/V1.Data_Analysis/describe.py
+++ src/V1.Data_Analysis/describe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from Descriptive_funtions import *
 
+
 def create_describe_file():
     data = pd.read_csv("./data/dataset_train.csv")
 
@@ -9,10 +10,10 @@ def create_describe_file():
 
     # Construct the final aggregation dictionary for ALL columns
     agg_list = [
-        ('count', lambda x: count(x)),      
-        ('mean', lambda x: mean(x)),        
-        ('std', lambda x: std(x)),          
-        ('min', lambda x: min(x)),          
+        ('count', lambda x: count(x)),
+        ('mean', lambda x: mean(x)),
+        ('std', lambda x: std(x)),
+        ('min', lambda x: min(x)),
         ('25%', lambda x: percentile(x, 25)),
         ('50%', lambda x: percentile(x, 50)),
         ('75%', lambda x: percentile(x, 75)),
@@ -23,17 +24,19 @@ def create_describe_file():
 
     results = {}
     for col in data_numeric.columns:
-        col_series = data_numeric[col].dropna() # Drop NaN before converting to list for clean calc
-        
-        col_results = {name: func(col_series.tolist()) for name, func in agg_list}
-        
+        # Drop NaN before converting to list for clean calc
+        col_series = data_numeric[col].dropna()
+
+        col_results = {name: func(col_series.tolist())
+                       for name, func in agg_list}
+
         results[col] = col_results
 
     # Convert the dictionary of results to a DataFrame
     result_df = pd.DataFrame(results)
-    
+
     print(result_df)
-    
-    
+
+
 if __name__ == "__main__":
     create_describe_file()
